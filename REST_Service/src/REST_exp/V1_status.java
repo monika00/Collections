48da1112ea506a0b546d9786a2fc56408832490b
monika00 <kazkurtoli1@gmail.com>
2015-09-07 13:50:00 +0300

    V1_Finished

--- REST_Service/src/REST_exp/V1_status.java
+++ REST_Service/src/REST_exp/V1_status.java
@@ -7,11 +7,20 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/V1/status")
 public class V1_status {
+	
+	private static final String Version = "1.0";
 
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle(){
 		return "<p>Java Web Service</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVerion(){
+		return "<p>Version:</p> " + Version;
 	}
 	
 }

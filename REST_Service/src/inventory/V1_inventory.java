12486aa47f78abf07df479708f4d7227633fc4d4
monika00 <kazkurtoli1@gmail.com>
2015-09-10 09:58:32 +0300

    ToJson complete with postgresql

---
+++ REST_Service/src/inventory/V1_inventory.java
@@ -0,0 +1,58 @@
+package inventory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.QueryParam;
+
+import org.codehaus.jettison.json.JSONArray;
+
+import DAO.DataBaseCon;
+import utility.ToJson;
+
+@Path("V1/inventory")
+public class V1_inventory {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String returnSQLData() throws Exception{
+		
+		PreparedStatement query = null;
+		Connection conn = null;
+		String returnString = null;
+		
+		try{
+			conn = DataBaseCon.DBConn();
+			query = conn.prepareStatement("select * from test.first");
+			ResultSet rs = query.executeQuery();
+			
+			//java.sql.ResultSetMetaData rsmd = rs.getMetaData();
+			
+			ToJson converter = new ToJson();
+			JSONArray json = new JSONArray();
+			
+			json = converter.toJSONArray(rs);
+			query.close();
+			
+			returnString = json.toString();
+			//returnString = rsmd.toString();
+		}
+		
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			if(conn != null) conn.close();
+		}
+		
+		return returnString;
+	}
+
+}
